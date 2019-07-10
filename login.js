9b03b649939b3c4a2402d025d758dd823ede01b8
ppeelman <philippe.peelman@gmail.com>
2019-07-10 13:04:49 +0200

    Buttons added login page

--- login.js
+++ login.js
@@ -1,4 +1,9 @@
 import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
 
 const username = new MDCTextField(document.querySelector('.username'));
 const password = new MDCTextField(document.querySelector('.password'));
+
+// Because we don't need to retain a reference to the ripple instance, there's no need to assign it to a variable.
+new MDCRipple(document.querySelector('.cancel'));
+new MDCRipple(document.querySelector('.next'));
